c6f6022bca07feeecff915477ecb3c608dc19e3d
josh fernandes <joshfernandes@mac.com>
2022-05-09 09:09:32 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='binding',
+    version='1.0.0',
+    packages=['binding'],
+    license='MIT',
+    author='Joshua M. Fernandes',
+    description='contains Observer meta class',
+    python_requires='>=3.6'
+)
